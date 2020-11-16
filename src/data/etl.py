50bce94110b58ecee01c07e4aba23f4920873d1a
Caiwei Wang <43483829+wendyw1999@users.noreply.github.com>
2020-11-15 23:26:25 -0800

    Update etl.py

--- src/data/etl.py
+++ src/data/etl.py
@@ -1,62 +1,78 @@
 import pandas as pd
 import numpy as np
-import os
-def pull_data():
+import scipy.linalg as la
+import matplotlib.pyplot as plt
+def get_data():
+    '''
+    retrieves information from the JHU and mobility data repository
+    '''
     url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
     us_confirmed_df = pd.read_csv(url, error_bad_lines=False)
-    us_confirmed_df.to_csv("data/temp/us_confirmed.csv")
-    print("Downloaded to data/temp/us_confirmed.csv")
-    
-    
-    
+
     url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv'
     us_death_df = pd.read_csv(url, error_bad_lines=False)
-    us_death_df.to_csv("data/temp/us_death.csv")
-    print("Downloaded to data/temp/us_death.csv")
+
+    display(us_confirmed_df.head())
+    display(us_death_df.head())
+
     url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv'
     global_recover_df = pd.read_csv(url, error_bad_lines=False)
-    global_recover_df.to_csv("data/temp/global_recover.csv")
-    print("Downloaded to data/temp/global_recover.csv")
+    display(global_recover_df.head())
+
 
     url = "https://raw.githubusercontent.com/descarteslabs/DL-COVID-19/master/DL-us-m50.csv"
     mobility = pd.read_csv(url, error_bad_lines=False)
-    mobility.to_csv("data/temp/mobility.csv")
-    print("Downloaded to data/temp/mobility.csv")
+    display(mobility.head())
+    us_confirmed_df = us_confirmed_df.loc[us_confirmed_df.Admin2 != "Unassigned"]
+    us_death_df = us_death_df.loc[us_death_df.Admin2 != "Unassigned"]
+    return us_confirmed_df,us_death_df,global_recover_df,mobility
 
-def download_data(data_param):
-    
-    
-    
-    
-    for i in data_param.keys():
-        url = data_param[i]
-        
-        df = pd.read_csv(url, error_bad_lines=False)
-        #do something here
-        print(df.head())
-        print("DataFrame :"+i)
 
-def clean_data(us_confirmed_df,us_death_df,global_recover_df):
-    # Data Preparation
-    us_confirmed = us_confirmed_df.drop(['UID', 'iso2', 'iso3', 'code3', 'FIPS', 'Admin2', 'Province_State',
+
+
+
+def get_county(start,days,county_code = 1001):
+    
+    county_confirmed = us_confirmed_df.loc[us_confirmed_df.FIPS == county_code]
+    .drop(['UID', 'iso2', 'iso3', 'code3', 'FIPS', 'Admin2', 'Province_State',
            'Country_Region', 'Lat', 'Long_', 'Combined_Key'],axis=1).sum(axis=0).values
-    us_death = us_death_df.drop(['UID', 'iso2', 'iso3', 'code3', 'FIPS', 'Admin2', 'Province_State',
+    county_death = us_death_df.loc[us_death_df.FIPS == county_code].
+    drop(['UID', 'iso2', 'iso3', 'code3', 'FIPS', 'Admin2', 'Province_State',
            'Country_Region', 'Lat', 'Long_', 'Combined_Key',"Population"],axis=1).sum(axis=0).values
-    us_recovered = global_recover_df.loc[global_recover_df["Country/Region"] == "US"].drop(
-        ["Province/State","Country/Region","Lat","Long"],axis= 1).values[0]
-    #us_removed = us_recovered+us_death
-    us_removed = us_recovered+us_death
-    us_population = us_death_df.Population.sum()
-    us_susceptible = us_population - us_confirmed
-    us_infected = us_confirmed
-
+    county_population = us_death_df.loc[us_death_df.FIPS ==county_code].Population.sum()
+    county_infected = county_confirmed
+    county_removed = county_death
+    county_susceptible = county_population - county_infected
+    return county_susceptible[start:days+start],county_infected[start:start+days],county_removed[start:start+days],county_population
+def get_state(start,days,state_name = "Washington"):
     
-    array = [us_removed, [us_population],us_susceptible,us_infected]
-    root = "data/temp/"
-    filenames = ["us_removed.txt", "us_population.txt","us_susceptible.txt","us_infected.txt"]
-        
-    for filename,my_list in list(zip(filenames,array)):
-        write_list_to_txt(root+filename,my_list)
+    county_confirmed = us_confirmed_df.loc[us_confirmed_df.Province_State == state_name].drop(['UID', 'iso2', 'iso3', 'code3', 'FIPS', 'Admin2', 'Province_State',
+           'Country_Region', 'Lat', 'Long_', 'Combined_Key'],axis=1).sum(axis=0).values
+    county_death = us_death_df.loc[us_death_df.Province_State == state_name].
+    drop(['UID', 'iso2', 'iso3', 'code3', 'FIPS', 'Admin2', 'Province_State',
+           'Country_Region', 'Lat', 'Long_', 'Combined_Key',"Population"],axis=1).
+    sum(axis=0).values
+    county_population = us_death_df.loc[us_death_df.Province_State ==state_name].Population.sum()
+    county_infected = county_confirmed
+    county_removed = county_death
+    county_susceptible = county_population - county_infected
+    return county_susceptible[start:start + days],county_infected[start:start + days],county_removed[start:days+start],county_population
+def get_country(start,days,country_name = "US"):
+    
+    county_confirmed = us_confirmed_df.loc[us_confirmed_df.Country_Region == country_name].
+    drop(['UID', 'iso2', 'iso3', 'code3', 'FIPS', 'Admin2', 'Province_State',
+           'Country_Region', 'Lat', 'Long_', 'Combined_Key'],axis=1).sum(axis=0).values
+    county_death = us_death_df.loc[us_death_df.Country_Region == country_name].
+    drop(['UID', 'iso2', 'iso3', 'code3', 'FIPS', 'Admin2', 'Province_State',
+           'Country_Region', 'Lat', 'Long_', 'Combined_Key',"Population"],axis=1).sum(axis=0).values
+    county_population = us_death_df.loc[us_death_df.Country_Region ==country_name].Population.sum()
+    country_recovered = global_recover_df.loc[global_recover_df["Country/Region"] == country_name].drop(
+    ["Province/State","Country/Region","Lat","Long"],axis= 1).values[0]
+    county_infected = county_confirmed
+    county_removed = county_death + country_recovered
+    county_susceptible = county_population - county_infected
+    return county_susceptible[start:start+days],county_infected[start:start+days],county_removed[start:start+days],county_population
+
 def write_list_to_txt(filename,my_list):
 
     with open(filename, 'w') as f:
